d54b6c62bfc4dae09eeb29ea95d97955850c44a6
MrVangonen <mr.vangonen@mail.ru>
2020-01-04 21:27:14 +0300

    navigatoin-drawer clear-fix; shedule card start

--- src/components/shedule/SheduleCard.vue
+++ src/components/shedule/SheduleCard.vue
@@ -1,5 +1,79 @@
 <template>
-  <div>
-    test
-  </div>
+    <div class="card">
+        <div class="my-row">
+            <span>
+                п/н
+            </span>
+            <span>
+                Время
+            </span>
+            <span>
+                Предмет
+            </span>
+            <span>
+                Вид занятия
+            </span>
+            <span>
+                Кабинет
+            </span>
+        </div>
+        <div class="my-row ">
+            <span>
+                {{ lesson.number }}
+            </span>
+            <span>
+                {{ lesson.time }}
+            </span>
+            <span>
+                {{ lesson.title }}
+            </span>
+            <span>
+                {{ lesson.type }}
+            </span>
+            <span> {{ lesson.cabinet }} {{ lesson.corps }} </span>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "sheduleCard",
+    props: {
+        lesson: {
+            number: Number,
+            time: Date,
+            title: String,
+            type: String,
+            existingPeriod: String,
+            cabinet: Number,
+            corps: String
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.crad {
+    display: flex;
+    flex-direction: row;
+}
+@media (min-width: 600px) {
+    .crad {
+        flex-direction: column;
+    }
+
+    .my-row {
+        & > :first-child {
+            width: calc(100vw / 5);
+        }
+        & > :not(:first-child) {
+            width: calc(100vw / 5);
+        }
+    }
+}
+.my-row {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow: hidden;
+}
+</style>
--- src/views/Shedule.vue
+++ src/views/Shedule.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="home">
-        <v-row align="baseline" justify="center" dense>
-            <v-col cols="12" sm="4">
+        <v-row align="baseline" dense>
+            <v-col xs12 sm6 md3>
                 <v-select
                     v-model="currentYear"
                     :items="years"
-                    value="title"
                     background-color="secondary lighten-3"
                     outlined
                     color="primary"
@@ -24,11 +23,16 @@
         </v-row>
         <v-row justify="center" dense>
             <v-col>
-                <v-expansion-panels focusable hover accordion>
-                    <v-expansion-panel v-for="(item, i) in 5" :key="i">
-                        <v-expansion-panel-header
-                            >Item</v-expansion-panel-header
-                        >
+                <v-expansion-panels
+                    focusable
+                    hover
+                    accordion
+                    v-model="currentDay"
+                >
+                    <v-expansion-panel v-for="(item, i) in days" :key="i">
+                        <v-expansion-panel-header class="text-uppercase">
+                            {{ item.text }}
+                        </v-expansion-panel-header>
                         <v-expansion-panel-content>
                             <!-- <div class="d-flex">
                               <div class="mr-2">
@@ -68,12 +72,7 @@
                                   1145
                                 </div>
                             </div> -->
-                            <v-data-table
-                                :headers="headers"
-                                :items="desserts"
-                                hide-default-footer
-                                hide-default-header
-                            ></v-data-table>
+                            <SheduleCard v-for="(item, i) in shedule" :key="i" :lesson="item" />
                         </v-expansion-panel-content>
                     </v-expansion-panel>
                 </v-expansion-panels>
@@ -90,6 +89,7 @@ export default {
     components: { SheduleCard },
     data: () => ({
         currentYear: 0,
+        currentDay: 0,
         years: [
             {
                 text: "2019/2020",
@@ -108,98 +108,37 @@ export default {
                 value: 3
             }
         ],
-        headers: [
+        days: [
             {
-                text: "Пара",
-                align: "left",
-                value: "count"
+                text: "30.12 Понедельник",
+                value: 0
             },
-            { text: "Время", value: "calories" },
-            { text: "", value: "fat" },
-            { text: "Carbs (g)", value: "carbs" },
-            { text: "Protein (g)", value: "protein" },
-            { text: "Iron (%)", value: "iron" }
+            {
+                text: "31.12 Вторник",
+                value: 1
+            },
+            {
+                text: "01.12 Среда",
+                value: 2
+            },
+            {
+                text: "02.12 Четверг",
+                value: 3
+            },
+            {
+                text: "03.12 Пятница",
+                value: 4
+            }
         ],
-        desserts: [
+        shedule: [
             {
-                name: "Frozen Yogurt",
-                calories: 159,
-                fat: 6.0,
-                carbs: 24,
-                protein: 4.0,
-                iron: "1%"
-            },
-            {
-                name: "Ice cream sandwich",
-                calories: 237,
-                fat: 9.0,
-                carbs: 37,
-                protein: 4.3,
-                iron: "1%"
-            },
-            {
-                name: "Eclair",
-                calories: 262,
-                fat: 16.0,
-                carbs: 23,
-                protein: 6.0,
-                iron: "7%"
-            },
-            {
-                name: "Cupcake",
-                calories: 305,
-                fat: 3.7,
-                carbs: 67,
-                protein: 4.3,
-                iron: "8%"
-            },
-            {
-                name: "Gingerbread",
-                calories: 356,
-                fat: 16.0,
-                carbs: 49,
-                protein: 3.9,
-                iron: "16%"
-            },
-            {
-                name: "Jelly bean",
-                calories: 375,
-                fat: 0.0,
-                carbs: 94,
-                protein: 0.0,
-                iron: "0%"
-            },
-            {
-                name: "Lollipop",
-                calories: 392,
-                fat: 0.2,
-                carbs: 98,
-                protein: 0,
-                iron: "2%"
-            },
-            {
-                name: "Honeycomb",
-                calories: 408,
-                fat: 3.2,
-                carbs: 87,
-                protein: 6.5,
-                iron: "45%"
-            },
-            {
-                name: "Donut",
-                calories: 452,
-                fat: 25.0,
-                carbs: 51,
-                protein: 4.9,
-                iron: "22%"
-            },
-            {
-                name: "KitKat",
-                calories: 518,
-                fat: 26.0,
-                carbs: 65,
-                protein: 7,
-                iron: "6%"
+                number: 6,
+                time: '17:00 - 18:35',
+                title: 'Английский язык',
+                type: 'Лекция',
+                existingPeriod: '17.09.19 - 18.01.20',
+                cabinet: 205,
+                corps: 'ГК'
             }
         ]
     }),
