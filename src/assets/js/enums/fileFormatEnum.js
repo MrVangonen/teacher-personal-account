b95e6ddf0b64c871036515dad336d53e1f707991
MrVangonen <mr.vangonen@mail.ru>
2020-02-22 21:43:56 +0300

    file loading: материалы занятия, прокачка markpicker

---
+++ src/assets/js/enums/fileFormatEnum.js
@@ -0,0 +1,34 @@
+const FileFormats = [
+    {
+        extensions: ["pdf"],
+        src: "pdf.png"
+    },
+    {
+        extensions: ["doc", "docx"],
+        src: "doc.png"
+    },
+    {
+        extensions: ["ppt"],
+        src: "ppt.png"
+    },
+    {
+        extensions: ["txt"],
+        src: "txt.png"
+    },
+    {
+        extensions: ["xlsx"],
+        src: "xlsx.png"
+    },
+    {
+        extensions: ["zip"],
+        src: "zip.png"
+    },
+    {
+        extensions: ["svg", "gif", "png", "jpg"],
+        src: "image.svg"
+    }
+];
+
+Object.freeze(FileFormats);
+
+export default FileFormats;
--- src/components/TheMarkPicker.vue
+++ src/components/TheMarkPicker.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <div class="my-2">
-            <div v-if="localMarks.length">
-                <v-icon
+        <div class="marks-container my-2">
+            <div class="marks" v-if="localMarks.length">
+                <v-hover
                     v-for="(mark, index) in localMarks"
                     :key="index"
-                    :color="mark.color"
-                    >{{ mark.icon }}</v-icon
+                    v-slot:default="{ hover }"
                 >
+                    <v-btn :color="mark.color" @click="deleteMark(index)" icon>
+                        <v-icon v-show="!hover">{{ mark.icon }} </v-icon>
+                        <v-icon v-show="hover">mdi-close</v-icon>
+                    </v-btn>
+                </v-hover>
             </div>
-            <div v-else>
+            <div class="marks" v-else>
                 Нет оценок
             </div>
         </div>
@@ -51,7 +55,7 @@ import markEnum from "@/assets/js/enums/markEnum.js";
 export default {
     data() {
         return {
-            markEnum: Object.values(markEnum),
+            markEnum: Object.values(markEnum)
         };
     },
     props: {
@@ -69,8 +73,25 @@ export default {
                 return value;
             }
         }
+    },
+    methods: {
+        deleteMark(index) {
+            this.localMarks.splice(index, 1);
+        }
     }
 };
 </script>
 
-<style></style>
+<style>
+.marks-container {
+    min-height: 72px;
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: center;
+}
+
+.marks-container .marks {
+    width: 100px;
+}
+</style>
